6c8af9a1ff5704dae164ea620ee23bbd966792ed
Oliverslife <lunar7s@naver.com>
2023-05-13 18:13:45 +0900

    23.05.13

--- contracts/MyContract.sol
+++
@@ -1,5 +0,0 @@
-pragma solidity ^0.8.19;
-
-contract MyContract {
-    string public myString = "Hello World!";
-}
---
+++ contracts/SimpleWallet.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.8.19;
+
+contract SimpleWallet {
+
+    address public owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(owner == msg.sender, "You are not allowed");
+        _;
+    }
+
+    function withdrawMondy(address payable _to, uint _amount) public onlyOwner {
+        _to.transfer(_amount);
+    }
+
+    function() external payable {
+
+    }
+}
